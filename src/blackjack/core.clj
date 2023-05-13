3caa5cbdfded6e59df105d75b7862ca3c94113f0
Henrique Jesus <henrique.jesus128@gmail.com>
2023-05-13 01:21:04 -0300

    refactor: create new-card and player

--- src/blackjack/core.clj
+++ src/blackjack/core.clj
@@ -5,3 +5,15 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn new-card []
+  (inc (rand-int 13)))
+
+(defn player [player-name]
+  (let [card-1 (new-card)
+        card-2 (new-card)]
+  {:player-name player-name
+   :cards [card-1, card-2]}))
+
+(println (player "Henrique"))
+(println (player "Dealer"))
