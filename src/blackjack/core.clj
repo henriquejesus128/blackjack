26c4cf61dec1358696ce999c7efa9723d58f30fa
Henrique Jesus <henrique.jesus128@gmail.com>
2023-05-15 10:21:38 -0300

    refactor: logic points and if

--- src/blackjack/core.clj
+++ src/blackjack/core.clj
@@ -9,17 +9,48 @@
 (defn new-card []
   (inc (rand-int 13)))
 
-(defn points-card [cards]
-  )
+(defn JQK->10 [card]
+  (if (> card 10)10 card))
+
+(defn A->11 [card]
+  (if (= card 1)11 card))
+
+(defn points-cards [cards]
+  (let [cards-JQK (map JQK->10 cards)
+    card-A11 (map A->11 cards-JQK)
+    points-A1 (reduce + cards-JQK)
+    points-A11 (reduce + card-A11)]
+  (if (> points-A11 21) points-A1 points-A11)))
 
 (defn player [player-name]
   (let [card-1 (new-card)
-        card-2 (new-card)
-        cards [card-1 card-2]
-        points (points-card cards)]
+    card-2 (new-card)
+    cards [card-1 card-2]
+    points (points-cards cards)]
   {:player-name player-name
    :cards cards
    :points points}))
 
-(println (player "Henrique"))
-(println (player "Dealer"))
+(defn get-card [player]
+  (let [card (new-card)
+    cards (conj (:cards player) card)
+    new-player (update player :cards conj card)
+    points (points-cards cards)]
+  (assoc new-player :points points)))
+
+(defn game [player]
+  (println (:player-name player) ":Mais carta ?")
+    (if(= (read-line) "sim")
+    (let [player-get-card (get-card player)]
+      (println player-get-card)
+      (game player-get-card))
+      player))
+
+(def player-1 (player "Henrique"))
+(println player-1)
+
+(def dealer (player "Dealer"))
+(println dealer)
+
+(game player-1)
+(game dealer)
